0a3212a055a95baafcb760f2b789e6442a991cff
thenicolas44 <nicolas_d_12@hotmail.com>
2023-05-12 00:48:12 -0500

    modificar redireccion de login

--- src/main/resources/static/js/src_login.js
+++ src/main/resources/static/js/src_login.js
@@ -2,15 +2,10 @@ function fnProcesaPaciente(comp){
     let id = comp.id;
     var escogido=id;
     window.location.href="http://localhost:8080/detalle/ola/ss/"+escogido;
-    
 }
 
 function fnInicioSession(){
     var usuario = document.getElementById("user").value;
     var contra = document.getElementById("password").value;
-    
-    window.location.href="http://localhost:8080/inicio";
-}
-function envio(){
-    window.location.href="http://localhost:8080/inicio";
+    window.location.href="http://localhost:8080/detalle/"+usuario+"/"+contra+"/1";
 }
