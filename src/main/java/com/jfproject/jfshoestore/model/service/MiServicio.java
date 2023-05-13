559220c0b2276d7e52f7e109e7cc95164df377f9
thenicolas44 <nicolas_d_12@hotmail.com>
2023-05-13 15:13:47 -0500

    correcciones de detalles

--- src/main/java/com/jfproject/jfshoestore/model/service/MiServicio.java
+++ src/main/java/com/jfproject/jfshoestore/model/service/MiServicio.java
@@ -14,6 +14,9 @@ public class MiServicio implements IServicio{
        if(element == 2){
         return "ADELE";
        }
+       if(element == 3){
+        return "ALBA";
+       }
        else{
         return "error elemento no encontrado";
        }
@@ -26,6 +29,9 @@ public class MiServicio implements IServicio{
            }
            if(element == 2){
             return "300";
+           }
+           if(element == 3){
+            return"219.00";
            }
            else{
             return "error elemento no encontrado";
@@ -40,6 +46,9 @@ public class MiServicio implements IServicio{
            if(element == 2){
             return "Zapatos ADELE para ocaciones especiales especialmente para ellos";
            }
+           if(element == 3){
+            return "Zapatos ALBA para ocaciones especiales especialmente para ellas";
+           }
            else{
             return "error elemento no encontrado";
            }
@@ -52,6 +61,9 @@ public class MiServicio implements IServicio{
            }
            if(element == 2){
             return "/assets/imagenes/portfolio/Adele_ProductoDestacado.jpg";
+           }
+           if(element == 3){
+            return "/assets/products/Alba.JPG";
            }
            else{
             return "/assets/imagenes/portfolio/not_found.png";
