3c396c016ed517f2bc9324dea50bf6fdc2885f34
Cristofer1201 <132797426+Cristofer1201@users.noreply.github.com>
2023-06-03 07:52:39 -0500

    Agregando controlador categoria

---
+++ src/main/java/com/jfproject/jfshoestore/controller/CategoriaController.java
@@ -0,0 +1,47 @@
+package com.jfproject.jfshoestore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jfproject.jfshoestore.model.Entity.CategoriaEntity;
+import com.jfproject.jfshoestore.model.service.ICategoriaService;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+    @Autowired
+    private ICategoriaService categoriaService;
+
+    @RequestMapping("/")
+    public String categoria(Model model){
+        CategoriaEntity categoria = new CategoriaEntity();
+        model.addAttribute("categoria", categoria);
+        model.addAttribute("listaCategorias", categoriaService.mostrarCategorias());
+        return "categoria";
+    }
+
+    @RequestMapping("/guardar")
+    public String guardar(CategoriaEntity categoria){
+        categoriaService.guardarCategoria(categoria);
+        return "redirect:/categoria/";
+    }
+
+    @RequestMapping("/mostrarEditar/{id}")
+    public String editar(@PathVariable (value = "id") Long id, Model model){
+        CategoriaEntity categoria = new CategoriaEntity();
+        categoria = categoriaService.buscarCategoria(id);
+        model.addAttribute("categoria", categoria);
+        model.addAttribute("listaCategorias", categoriaService.mostrarCategorias());
+        return "categoria";
+    }
+    
+
+    @RequestMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable (value = "id") Long id){
+        categoriaService.eliminarCategoria(id);
+        return "redirect:/categoria/";
+    }
+}
