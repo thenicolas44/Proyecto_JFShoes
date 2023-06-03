783605999458022db04ba090bc9064be02380d29
Meysoner <132797346+Meysoner@users.noreply.github.com>
2023-06-03 03:09:04 -0500

    Creación del UsuarioServiceImp.java

---
+++ src/main/java/com/jfproject/jfshoestore/model/service/UsuarioServiceImp.java
@@ -0,0 +1,37 @@
+package com.jfproject.jfshoestore.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jfproject.jfshoestore.model.Entity.UsuarioEntity;
+import com.jfproject.jfshoestore.model.dao.IUsuario_ADao;
+
+@Service
+public class UsuarioServiceImp implements IUsuarioService{
+
+    @Autowired
+    private IUsuario_ADao usuarioDAO;
+
+    @Override
+    public void guardarUsuario(UsuarioEntity usuario) {
+        usuarioDAO.save(usuario);
+    }
+
+    @Override
+    public List<UsuarioEntity> mostrarUsuarios() {
+        return (List<UsuarioEntity>)usuarioDAO.findAll();
+    }
+
+    @Override
+    public UsuarioEntity buscarUsuario(Long id) {
+        return usuarioDAO.findById(id).orElse(null);
+    }
+
+    @Override
+    public void eliminarUsuario(Long id) {
+        usuarioDAO.deleteById(id);
+    }
+    
+}
