73881e3d237f5d59d4b1a2c9a935c9ae6f29c6ab
Meysoner <132797346+Meysoner@users.noreply.github.com>
2023-06-03 03:09:42 -0500

    Creación del IUsuarioService.java

---
+++ src/main/java/com/jfproject/jfshoestore/model/service/IUsuarioService.java
@@ -0,0 +1,12 @@
+package com.jfproject.jfshoestore.model.service;
+
+import java.util.List;
+
+import com.jfproject.jfshoestore.model.Entity.UsuarioEntity;
+
+public interface IUsuarioService {
+    public void guardarUsuario(UsuarioEntity usuario);
+    public List<UsuarioEntity> mostrarUsuarios();
+    public UsuarioEntity buscarUsuario(Long id);
+    public void eliminarUsuario(Long id);
+}
