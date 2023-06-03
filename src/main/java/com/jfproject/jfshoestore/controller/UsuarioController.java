839159e01716b81987287a6f3522be2af1344619
Meysoner <132797346+Meysoner@users.noreply.github.com>
2023-06-03 03:03:57 -0500

    Creación del UsuarioController.java

---
+++ src/main/java/com/jfproject/jfshoestore/controller/UsuarioController.java
@@ -0,0 +1,50 @@
+package com.jfproject.jfshoestore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jfproject.jfshoestore.model.Entity.UsuarioEntity;
+import com.jfproject.jfshoestore.model.service.IUsuarioService;
+
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    
+    @Autowired
+    private IUsuarioService usuarioService;
+
+    @RequestMapping("/")
+    public String inicio(Model modeL){
+        UsuarioEntity usuario = new UsuarioEntity();
+        modeL.addAttribute("usuario", usuario);
+        modeL.addAttribute("titulo", "Registrar Nuevo Usuario");
+        modeL.addAttribute("listaUsuarios", usuarioService.mostrarUsuarios());
+        return "usuario/index";
+    }
+
+    @RequestMapping("/guardar")
+    public String guardar(UsuarioEntity usuario){
+        usuarioService.guardarUsuario(usuario);
+        return "redirect:/usuarios/";
+    }
+
+    @RequestMapping("/mostrarEditar/{id}")
+    public String editar(@PathVariable(value="id") Long id, Model modeL) {
+        UsuarioEntity usuario = new UsuarioEntity();
+        usuario = usuarioService.buscarUsuario(id);
+        modeL.addAttribute("usuario", usuario);
+        modeL.addAttribute("titulo", "Editar Usuario");
+        modeL.addAttribute("listaUsuarios", usuarioService.mostrarUsuarios());
+        return "usuario/index";
+    }
+    
+    @RequestMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable(value="id") Long id){
+        usuarioService.eliminarUsuario(id);
+        return "redirect:/usuarios/";
+    }
+}
