4851d19eb2055d1bf23d92ce6178309589743937
thenicolas44 <nicolas_d_12@hotmail.com>
2023-06-07 20:10:41 -0500

    arreglos

--- src/main/java/com/jfproject/jfshoestore/model/dao/IUsuarioDao.java
+++ src/main/java/com/jfproject/jfshoestore/model/dao/IUsuarioDao.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import com.jfproject.jfshoestore.model.Entity.UsuarioEntity;
 
 public interface IUsuarioDao extends CrudRepository<UsuarioEntity,Long>{
-    @Query(value = "SELECT * FROM USUARIO WHERE USER_NICKNAME = :nickname", nativeQuery = true)
+    @Query(value = "SELECT * FROM USUARIOS WHERE USER_NICKNAME = :nickname", nativeQuery = true)
     UsuarioEntity findbyname(@Param("nickname") String nombre);
 
 }
