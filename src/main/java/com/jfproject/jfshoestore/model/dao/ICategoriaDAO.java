daf5c721eef6541b03ef59500828f674509e2e0c
thenicolas44 <132797181+thenicolas44@users.noreply.github.com>
2023-06-07 20:12:43 -0500

    Delete ICategoriaDAO.java

--- src/main/java/com/jfproject/jfshoestore/model/dao/ICategoriaDAO.java
+++
@@ -1,8 +0,0 @@
-package com.jfproject.jfshoestore.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.jfproject.jfshoestore.model.Entity.CategoriaEntity;
-
-public interface ICategoriaDAO extends CrudRepository<CategoriaEntity,Long>{
-    
-}
