daa97f9e81e79c3670e3515a2782d9c51654687a
thenicolas44 <nicolas_d_12@hotmail.com>
2023-06-07 20:23:11 -0500

    listo

---
+++ src/main/java/com/jfproject/jfshoestore/model/dao/ICategoriaDao.java
@@ -0,0 +1,9 @@
+package com.jfproject.jfshoestore.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jfproject.jfshoestore.model.Entity.CategoriaEntity;
+
+public interface ICategoriaDao extends CrudRepository<CategoriaEntity,Long>{
+    
+}
