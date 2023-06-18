a1e4886cb75a61a03873ed79bc954f7ca7109616
Cristofer1201 <132797426+Cristofer1201@users.noreply.github.com>
2023-06-17 23:40:19 -0500

    Añadiendo busqueda de voz js

---
+++ src/main/resources/static/js/busquedaVoz.js
@@ -0,0 +1,18 @@
+const startBtn = document.getElementById('btn-voz');
+const input = document.getElementById('result');
+const recognition = new webkitSpeechRecognition(); // Inicializa el objeto de reconocimiento de voz
+recognition.lang = 'es-ES'; // Establece el idioma a español
+//recognition.continuous = true; // Permite el reconocimiento continuo
+//recognition.interimResults = true; // Proporciona resultados intermedios
+startBtn.addEventListener('click', () => {     
+        confirm = recognition.start(); // Inicia el reconocimiento de voz cuando se hace clic en el botón
+});
+    
+recognition.addEventListener('result', (event) => {
+    const transcript = event.results[0][0].transcript; // Obtiene el texto reconocido
+    input.value = transcript; // Muestra el texto reconocido en el elemento 'result'
+    if((transcript==="zapatilla Alba") || (transcript==="zapatilla Alba") || (transcript==="Alba")){
+        window.location.href = "http://localhost:8080/jf-store/detalle/3/";
+    }
+});
+
