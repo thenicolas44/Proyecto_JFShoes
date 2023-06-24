26724c4a7780713480374eb15bdf94b43e64e734
thenicolas44 <nicolas_d_12@hotmail.com>
2023-06-24 18:43:47 -0500

    arreglo modal producto terminado

--- src/main/resources/static/js/ModalsMantenimiento.js
+++ src/main/resources/static/js/ModalsMantenimiento.js
@@ -6,6 +6,9 @@
             document.getElementById('cboCategoria').value = item.dataset.categoria;
             document.getElementById('txtNombre').value = item.dataset.nombre;
             document.getElementById('txtPrecio').value = item.dataset.precio;
+            document.getElementById('txtDescripcion').value = item.dataset.descripcion;
+            document.getElementById('txtTalla').value = item.dataset.talla;
+            document.getElementById('txtColor').value = item.dataset.color;
             new bootstrap.Modal(document.getElementById('modalEditarProducto')).show();
         });
     });
