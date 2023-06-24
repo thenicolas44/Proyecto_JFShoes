a6c8b8d56b08403cabc8176a16de2679b1ad7ea5
thenicolas44 <nicolas_d_12@hotmail.com>
2023-06-24 00:40:32 -0500

    arreglos en voz

--- src/main/resources/static/js/busquedaVoz.js
+++ src/main/resources/static/js/busquedaVoz.js
@@ -5,13 +5,15 @@ recognition.lang = 'es-ES'; // Establece el idioma a español
 //recognition.continuous = true; // Permite el reconocimiento continuo
 //recognition.interimResults = true; // Proporciona resultados intermedios
 startBtn.addEventListener('click', () => {     
-        confirm = recognition.start(); // Inicia el reconocimiento de voz cuando se hace clic en el botón
+    confirm = recognition.start(); // Inicia el reconocimiento de voz cuando se hace clic en el botón
 });
-    
 recognition.addEventListener('result', (event) => {
     const transcript = event.results[0][0].transcript; // Obtiene el texto reconocido
-    input.value = transcript; // Muestra el texto reconocido en el elemento 'result'
-    if((transcript==="zapatilla Alba") || (transcript==="zapatilla Alba") || (transcript==="Alba")){
+    var transcriptSinPunto = transcript.replace(/\./g, '');
+    input.value = transcriptSinPunto; 
+    transcriptSinPunto = transcriptSinPunto.toUpperCase();
+    // Muestra el texto reconocido en el elemento 'result'
+    if((transcriptSinPunto==="ZAPATILLA ALBA") || (transcriptSinPunto==="Zapatilla Alba") || (transcriptSinPunto==="Alba")){
         window.location.href = "http://localhost:8080/jf-store/detalle/3/";
     }
 });
