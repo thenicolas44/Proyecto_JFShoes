f62815ce4141a90e25018e0b59cbaadb72465e36
JheanderFC <132981499+JheanderFC@users.noreply.github.com>
2023-07-07 01:05:11 -0500

    Update UsuarioController.java
    Importaciones jf jijiji - Jheandersito

--- src/main/java/com/jfproject/jfshoestore/controller/UsuarioController.java
+++ src/main/java/com/jfproject/jfshoestore/controller/UsuarioController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.jfproject.jfshoestore.model.Entity.UsuarioEntity;
 import com.jfproject.jfshoestore.model.service.IUsuarioService;
+
 
 
 @Controller
