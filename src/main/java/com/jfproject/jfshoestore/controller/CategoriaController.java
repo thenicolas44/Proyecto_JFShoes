09a3347f7894617ccd925be74cc618c88fb6ab08
JheanderFC <132981499+JheanderFC@users.noreply.github.com>
2023-07-07 00:52:33 -0500

    Update CategoriaController.java
    Agregacion del Import que me olvide - Jheandersito

--- src/main/java/com/jfproject/jfshoestore/controller/CategoriaController.java
+++ src/main/java/com/jfproject/jfshoestore/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.jfproject.jfshoestore.model.Entity.CategoriaEntity;
 import com.jfproject.jfshoestore.model.service.ICategoriaService;
