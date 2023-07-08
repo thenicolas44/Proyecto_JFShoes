e7fb45c7106775359909fd6a178a3bd7be830dd7
Cristofer1201 <132797426+Cristofer1201@users.noreply.github.com>
2023-07-07 22:27:22 -0500

    Add files via upload

---
+++ src/main/java/com/jfproject/jfshoestore/model/dao/ICarroCompraDao.java
@@ -0,0 +1,9 @@
+package com.jfproject.jfshoestore.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jfproject.jfshoestore.model.Entity.CarroCompraEntity;
+
+public interface ICarroCompraDao extends CrudRepository<CarroCompraEntity,Long>{
+    
+}
